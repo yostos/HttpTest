4a06ac0ea3b6b7ff55f8d4020b59a101715c3805
Yoshida Toshiyuki <yostos@yostos.org>
2014-12-16 18:01:25 +0900

    Create BluepageLookupCondition class

--- HttpTest/BluepageLookupCondition.swift
+++ HttpTest/BluepageLookupCondition.swift
@@ -9,5 +9,42 @@
 import Foundation
 class BluepageLookupCondition : NSObject {
     
+    // These fields are key for looking up Bluepages
+    // 'nil' means that the key is not speciflied.
+    
+    var name : NSString?
+    var building : NSString?
+    var dept : NSString?
+    var empno : NSString?
+    var noteId : NSString?
+    var shortName : NSString?
+    var tel : NSString?
+    var IsManager : Bool
+    
+    
+    override init() {
 
+        self.IsManager = false
+        self.name = nil
+        self.building = nil
+        self.dept = nil
+        self.empno = nil
+        self.noteId  = nil
+        self.shortName  = nil
+        self.tel  = nil
+        self.IsManager = false
+        super.init()
+    }
+    
+    func clear() {
+        self.IsManager = false
+        self.name = nil
+        self.building = nil
+        self.dept = nil
+        self.empno = nil
+        self.noteId  = nil
+        self.shortName  = nil
+        self.tel  = nil
+        self.IsManager = false
+    }
 }
