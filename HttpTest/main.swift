70fba366da9ad5bd9d605aeb10d867a8fad40e32
Yoshida Toshiyuki <yostos@yostos.org>
2014-12-16 12:07:11 +0900

    Successfully convert NSData to NSString

--- HttpTest/main.swift
+++ HttpTest/main.swift
@@ -8,15 +8,26 @@
 
 import Foundation
 
-println("Hello, World!")
+
+
+import Foundation
+
+let arr = [
+    0xe3, 0x81, 0x82,
+    0xe3, 0x81, 0x84,
+    0xe3, 0x81, 0x86,
+    0xe3, 0x81, 0x88,
+    0xe3, 0x81, 0x8a
+    ] as [CUnsignedChar]
+
+let datax = NSData(bytes: arr, length: 15)
+let str = NSString(data:datax, encoding:NSUTF8StringEncoding)
+
+println(str)
+
+
 var request = NSURLRequest(URL: NSURL(string: "http://bluepages.ibm.com/BpHttpApisv3/wsapi?bySerial=096826")!)
-var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
-if data == nil {
-    println("No Data Found")
-}
-else {
-    println(data)
-  //  var string:NSString!
-  //  string = init(data: data,encoding: NSUTF8StringEncoding)
-    
- }
+let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
+
+let string:NSString = NSString(data: data!,encoding: NSUTF8StringEncoding)!
+println(string)
